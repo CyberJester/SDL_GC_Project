91c3cd5d352187be0eed4bf025dc5885328ff5e2
CyberJester <jester.development@gmail.com>
2025-01-12 14:19:54 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System; 
+using SDL2;
+using GameControls;
+using GameControls.Managers;
+
+namespace GameControlsTest 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            using (var game = new TestGame()) 
+            { 
+                game.Run(); 
+            }
+        } 
+    } 
+}
